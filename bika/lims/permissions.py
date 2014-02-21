b1df5f25b351ac9a40388c894045fc9734b62d83
Campbell <campbell@bikalabs.com>
2014-02-21 17:37:39 +0200

    alspec-s6 merged and rebased.

--- bika/lims/permissions.py
+++ bika/lims/permissions.py
@@ -147,3 +147,7 @@ ReopenBatch = 'BIKA: Reopen Batch'
 # Manage AR Imports
 # ----------------------------------------------
 ManageARImport = 'BIKA: Manage ARImport'
+
+# Manage AR Priorities
+# ----------------------------------------------
+ManageARPriority = 'BIKA: Manage ARPriority'
